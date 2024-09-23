802b82babf89f0dccb71207c472f57f69947c613
chairul <chairul.amri@ebconnect.com>
2024-09-23 19:57:49 +0700

    add notes values using push()

--- src/handler.js
+++ src/handler.js
@@ -1,12 +1,23 @@
 /*eslint linebreak-style: ["error", "windows"]*/
 const { nanoid } = require('nanoid');
+const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
   const { title, tags, body } = request.payload;
-
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
   const updateAt = createdAt;
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updateAt,
+  };
+
+  notes.push(newNote);
 };
 
 module.exports = { addNoteHandler };
